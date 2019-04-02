6e5e9855d49f1b2220fad0fe51eae73bdf302b16
jiuqu <qc_8431d2a911e44ac48dcec09040620566@git.code.tencent.com>
2019-04-02 18:24:30 +0800

    m

---
+++ Assets/Scipts/FoodMaker.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FoodMaker : MonoBehaviour
+{
+    public int xlimit = 18;
+    public int ylimit = 12;
+    public int stepLenth = 30;
+
+    public GameObject foodPrefab;
+    public Sprite[] foodSprite;
+
+    private Transform foodHolder;
+
+    private static FoodMaker _instance;
+    public static FoodMaker Instance()
+    {
+        return _instance;
+    }
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foodHolder = GameObject.Find("FoodHolder").transform;
+        MakeFood();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MakeFood()
+    {
+        int index = Random.Range(0, foodSprite.Length);
+        GameObject food = Instantiate(foodPrefab);
+        food.GetComponent<Image>().sprite = foodSprite[index];
+        food.transform.SetParent(foodHolder, false);
+        int x = Random.Range(-xlimit, xlimit);
+        int y = Random.Range(-ylimit, ylimit);
+        food.transform.localPosition = new Vector3(x * 30, y * 30, 0);
+    }
+}
---
+++ Assets/Scipts/SnakeHead.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq; // List.Last()
+
+public class SnakeHead : MonoBehaviour
+{
+    // 多久调用一次
+    public float velocity = 0.35f;
+    public int step;
+    private int x;
+    private int y;
+    private Vector3 headPos;
+
+    // 蛇身
+    public List<Transform> bodyList = new List<Transform>();
+
+    // 蛇身预制体
+    public GameObject bodyPrefab;
+    public Sprite[] bodySprites = new Sprite[2];
+
+    // canvas
+    private Transform canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = GameObject.Find("Canvas").transform;
+
+        // 重复调用
+        InvokeRepeating("Move", 0, velocity);
+        x = step;
+        y = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CancelInvoke();
+            InvokeRepeating("Move", 0, velocity - 0.2f);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            CancelInvoke();
+            InvokeRepeating("Move", 0, velocity);
+        }
+        if (Input.GetKey(KeyCode.W) && y != -step)
+        {
+            x = 0;
+            y = step;
+            //gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.S) && y != step)
+        {
+            x = 0;
+            y = -step;
+        }
+        if (Input.GetKey(KeyCode.A) && x != step)
+        {
+            x = -step;
+            y = 0;
+        }
+        if (Input.GetKey(KeyCode.D) && x != -step)
+        {
+            x = step;
+            y = 0;
+        }
+    }
+
+    void Move()
+    {
+        headPos = gameObject.transform.localPosition;
+        gameObject.transform.localPosition = new Vector3(headPos.x + x, headPos.y + y, headPos.z);
+        if (bodyList.Count > 0)
+        {
+            bodyList.Last().localPosition = headPos;
+            bodyList.Insert(0, bodyList.Last());
+            bodyList.RemoveAt(bodyList.Count - 1);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // 食物
+        if (collision.tag == "Food")
+        {
+            Destroy(collision.gameObject);
+            Grow();
+            FoodMaker.Instance().MakeFood();
+        }
+        // 身体
+        else if (collision.tag == "Body")
+        {
+            Debug.Log("Body Die....");
+        }
+        // 边界
+        else
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Up":
+                    transform.localPosition = new Vector3(transform.localPosition.x, -transform.localPosition.y + 30, transform.localPosition.z);
+                    Debug.Log("Up Die....");
+                    break;
+                case "Down":
+                    transform.localPosition = new Vector3(transform.localPosition.x, -transform.localPosition.y - 30, transform.localPosition.z);
+                    Debug.Log("Down Die....");
+                    break;
+                case "Left":
+                    transform.localPosition = new Vector3(-transform.localPosition.x - 30, transform.localPosition.y, transform.localPosition.z);
+                    Debug.Log("Left Die....");
+                    break;
+                case "Right":
+                    transform.localPosition = new Vector3(-transform.localPosition.x + 30, transform.localPosition.y, transform.localPosition.z);
+                    Debug.Log("Right Die....");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void Grow()
+    {
+        int index = (bodyList.Count % 2 == 0) ? 0 : 1; 
+        GameObject body = Instantiate(bodyPrefab, new Vector3(2000, 2000, 0), Quaternion.identity);
+        body.GetComponent<Image>().sprite = bodySprites[index];
+
+        // false:不自动保存它的世界坐标
+        body.transform.SetParent(canvas, false);
+
+        bodyList.Add(body.transform);
+    }
+}
